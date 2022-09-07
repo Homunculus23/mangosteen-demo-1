8938aa9404cd6a2b1e1d885a9dfa9f9670d1e110
fpatr <fpatr@qq.com>
2022-09-07 13:56:05 +0800

    删除无用文件

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mangosteen-demo-1/dist/',  //build path
+  // base: '/mangosteen-demo-1/dist/',  //build path
   plugins: [vue()]
 })
