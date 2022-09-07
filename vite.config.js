7259a9c4d6abb0f66fb2326f350dba1ae4fe1c10
fpatr <fpatr@qq.com>
2022-09-07 13:28:20 +0800

    重新build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/',  //build path
+  base: '/mangosteen-demo-1/dist/',  //build path
   plugins: [vue()]
 })
