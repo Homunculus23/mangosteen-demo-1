42facbd3aa79e04ee0170b46f7895bfd385150f0
fpatr <fpatr@qq.com>
2022-09-19 15:26:42 +0800

    四个欢迎页面搞定

--- src/components/welcome/Forth.jsx
+++ src/components/welcome/Forth.jsx
@@ -1,11 +1,21 @@
 import { defineComponent} from "vue";
-
+import s from './First.module.scss';
+import cloud from '../../assets/icons/cloud.svg'
+import { RouterLink } from "vue-router";
 
 export const Forth = defineComponent({
     setup: (props, context) => {
         return () => (
-            <div>
-                Forth
+            <div class={s.wrapper}>
+                <div class={s.card}>
+                    <img src={cloud}/>
+                    <h2>每日提醒<br/>不遗漏每一笔账单</h2>
+                </div>
+                <div className={s.actions}>
+                    <RouterLink class={s.fake} to="/welcome/2">占位</RouterLink>
+                    <RouterLink to="/start">完成</RouterLink>
+                    <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+                </div>
             </div>
         );
     }
